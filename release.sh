dd15fb7df4edef8b21ae299d7e8c40379506990f
xinbg <biguo_xin@pinssible.com>
2018-03-09 16:48:10 +0800

    remove exec

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ read -p '>' new_version
 echo 'Please input release message'
 read -p '>' new_message
 echo 'Tagging current version as v'$new_version
-exec git tag -a v$new_version -m "$new_message"
+git tag -a v$new_version -m "$new_message"
 echo 'Pushing tag...'
-exec git push origin v$new_version
-echo 'Relasing...'
-exec goreleaser --rm-dist
+git push origin v$new_version
+echo 'Releasing...'
+goreleaser --rm-dist
